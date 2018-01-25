102415270a3e736dfa9754b3b0c1756290e1dffc
Sheri Wilson <smgwilson37@gmail.com>
2018-01-24 21:35:29 -0800

    WIP

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,14 +3,14 @@ watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
-  
+
   browserSync.init({
     notify: false,
     server: {
       baseDir: "app"
     }
   });
-  
+
   watch('./app/index.html', function() {
     browserSync.reload();
   });
@@ -18,9 +18,17 @@ gulp.task('watch', function() {
   watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
+
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
 });
 
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src("./app/temp/styles/styles.css")
     .pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
+})
